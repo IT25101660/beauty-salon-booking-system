19451ebd7632ca01c2af8f2fe2792d8c007246cc
IT25101660 <it25101660@my.sliit.lk>
2026-04-12 14:21:45 +0530

    added file handling to save reviews into reviews.txt

--- src/ReviewController.java
+++ src/ReviewController.java
@@ -1,8 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReviewController {
     private List<Review> reviews;
+    private final String FILE_NAME = "reviews.txt";
 
     public ReviewController() {
         reviews = new ArrayList<>();
@@ -10,7 +14,21 @@ public class ReviewController {
 
     public void addReview(Review review) {
         reviews.add(review);
+        saveReviewToFile(review);
         System.out.println("Review added successfully.");
+    }
+
+    private void saveReviewToFile(Review review) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {
+            writer.write(review.getReviewId() + "," +
+                    review.getCustomerName() + "," +
+                    review.getServiceName() + "," +
+                    review.getRating() + "," +
+                    review.getComment());
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
     }
 
     public void viewAllReviews() {
