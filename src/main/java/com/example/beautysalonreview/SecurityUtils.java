7041a938800deb9edc53526dd253755e75d7ac46
IT25101660 <it25101660@my.sliit.lk>
2026-05-12 23:57:04 +0530

    Remove unnecessary files

--- src/main/java/com/example/beautysalonreview/SecurityUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.beautysalonreview;
-
-import jakarta.servlet.http.HttpSession;
-
-public class SecurityUtils {
-    public static boolean isManager(HttpSession session) {
-        return "MANAGER".equals(session.getAttribute("staffRole"));
-    }
-}
